2eae2e968281786b0f1aaf185dc4db30bf8fe27d
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 20:53:59 +0700

    tambah fungsi view,detail,add,edit

--- app/Http/Controllers/data_pengguna/GuruController.php
+++ app/Http/Controllers/data_pengguna/GuruController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class GuruController extends Controller
 {
-    //
+     //
+     public function guruView () {
+        return view("tampilan.data_pengguna.guru.view_guru");
+    }
+
+    public function guruAdd () {
+        return view("tampilan.data_pengguna.guru.add_guru");
+    }
+    public function guruDetail () {
+        return view("tampilan.data_pengguna.guru.detail_guru");
+    }
+
+    public function guruEdit () {
+        return view("tampilan.data_pengguna.guru.Edit_guru");
+    }
 }
