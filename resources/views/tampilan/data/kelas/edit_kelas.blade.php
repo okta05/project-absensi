db1430c43e77642a90568e5ef81619ef4665be71
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 21:04:44 +0700

    tambah halaman view, add, edit kelas

---
+++ resources/views/tampilan/data/kelas/edit_kelas.blade.php
@@ -0,0 +1,66 @@
+@extends('tampilan.index_master')
+@section('tampilan')
+
+<div class="pagetitle">
+    <h1>Ubah Data Kelas</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route("dashboard")}}"><i class="bi bi-house-door-fill"></i></a></li>
+            <li class="breadcrumb-item"><a href="{{route("kelas.view")}}">Data Kelas</a></li>
+            <li class="breadcrumb-item active"><a href="#">Ubah Data Kelas</a></li>
+        </ol>
+    </nav>
+</div><!-- End Page Title -->
+
+<div class="col-lg-12">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Form Ubah Kelas</h5>
+
+            <!-- General Form Elements -->
+            <form>
+                <div class="row mb-3">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama Kelas</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" placeholder="Masukkan nama lengkap">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label class="col-sm-2 col-form-label">Tingkat</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected disabled>Pilih Tingkat Telas</option>
+                            <option value="1">7</option>
+                            <option value="2">8</option>
+                            <option value="2">8</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label class="col-sm-2 col-form-label">Wali Kelas</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected disabled>pilih Wali Kelas</option>
+                            <option value="1">Wali Kelas 1</option>
+                            <option value="2">Wali Kelas 2</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <a href="{{route('kelas.view')}}" class="btn btn-success">Batal</a>
+                    </div>
+                </div>
+
+            </form><!-- End General Form Elements -->
+
+        </div>
+    </div>
+
+</div>
+
+@endsection
---
+++ resources/views/tampilan/data/kelas/view_kelas.blade.php
@@ -0,0 +1,65 @@
+@extends('tampilan.index_master')
+@section('tampilan')
+
+<div class="pagetitle">
+    <h1>Data Kelas</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route("dashboard")}}"><i class="bi bi-house-door-fill"></i></a></li>
+            <li class="breadcrumb-item active"><a href="#">Data Kelas</a></li>
+    </nav>
+</div>
+<!-- nnd Page Title -->
+
+<div class="row mb-3">
+    <!-- Button to add ticket -->
+    <div class="col text-start">
+        <a class="btn btn-success" href="{{ route('kelas.add') }}">
+            <i class="bi bi-plus-square"></i>
+        </a>
+    </div>
+</div>
+
+<section class="section">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <!-- Table with stripped rows -->
+                        <table class="table table-striped datatable">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Kelas</th>
+                                    <th>Wali Kelas</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Data siswa ditampilkan di sini -->
+                                <tr>
+                                    <td>1</td>
+                                    <td>VII A</td>
+                                    <td>Oktaviano</td>
+                                    <td>
+                                        <div class="dropdown">
+
+                                            <a class="btn btn-warning" href="{{ route('kelas.edit') }}"><i
+                                                    class="bi bi-pencil-square"></i></a>
+
+                                            <a class="btn btn-danger" href="#"><i class="bi bi-trash"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- End Table with stripped rows -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
