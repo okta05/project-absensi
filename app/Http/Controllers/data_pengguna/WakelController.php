855e2171b896325599655cd9156f573df99cdf82
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 15:05:10 +0700

    Membuat Controller kepsek, kurikulum, wakel, bk, guru

---
+++ app/Http/Controllers/data_pengguna/WakelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\data_pengguna;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WakelController extends Controller
+{
+    //
+}
