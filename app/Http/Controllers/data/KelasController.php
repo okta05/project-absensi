0530f368d2a9b22572e37315e2c0be013b05e56b
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 21:06:13 +0700

    tambah fungsi view, add, edit pada controller kelas

--- app/Http/Controllers/data/KelasController.php
+++ app/Http/Controllers/data/KelasController.php
@@ -8,4 +8,15 @@ use Illuminate\Http\Request;
 class KelasController extends Controller
 {
     //
+    public function kelasView() {
+        return view("tampilan.data.kelas.view_kelas");
+    }
+
+    public function kelasAdd() {
+        return view("tampilan.data.kelas.add_kelas");
+    }
+
+    public function kelasEdit() {
+        return view("tampilan.data.kelas.edit_kelas");
+    }
 }
