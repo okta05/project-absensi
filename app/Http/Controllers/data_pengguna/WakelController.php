ac3c960f48d1aa69b7ebf5deb48b329e29fd56c5
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 20:43:34 +0700

    tambah fungsi view,detail,add,edit pada controller wakel

--- app/Http/Controllers/data_pengguna/WakelController.php
+++ app/Http/Controllers/data_pengguna/WakelController.php
@@ -8,4 +8,18 @@ use Illuminate\Http\Request;
 class WakelController extends Controller
 {
     //
+    public function wakelView () {
+        return view("tampilan.data_pengguna.wakel.view_wakel");
+    }
+
+    public function wakelAdd () {
+        return view("tampilan.data_pengguna.wakel.add_wakel");
+    }
+    public function wakelDetail () {
+        return view("tampilan.data_pengguna.wakel.detail_wakel");
+    }
+
+    public function wakelEdit () {
+        return view("tampilan.data_pengguna.wakel.Edit_wakel");
+    }
 }
