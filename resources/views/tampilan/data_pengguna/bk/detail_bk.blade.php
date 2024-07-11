2161618474ea8bb1d2a213e9fae99a0811801085
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-11 20:28:05 +0700

    tambah halaman add,edit,detail,edit

---
+++ resources/views/tampilan/data_pengguna/bk/detail_bk.blade.php
@@ -0,0 +1,84 @@
+@extends('beranda.index_master')
+@section('beranda')
+
+<div class="pagetitle">
+    <h1>Detail Data BK</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route("dashboard")}}"><i class="bi bi-house-door-fill"></i></a></li>
+            <li class="breadcrumb-item"><a href="{{route("bk.view")}}">Data BK</a></li>
+            <li class="breadcrumb-item active"><a href="#">Detail Data BK</a></li>
+        </ol>
+    </nav>
+</div><!-- End Page Title -->
+
+<section class="section profile">
+    <div class="row">
+        <div class="col-xl-4">
+
+            <div class="card">
+                <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+                    <img src="{{asset('backend/assets/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
+                    <h2>Oktaviano Kurniawan</h2>
+                    <h3>NIP</h3>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-xl-8">
+
+            <div class="card">
+                <div class="card-body pt-3">
+                    <!-- Bordered Tabs -->
+                    <ul class="nav nav-tabs nav-tabs-bordered">
+
+                        <li class="nav-item">
+                            <button class="nav-link active" data-bs-toggle="tab"
+                                data-bs-target="#profile-overview">Detail</button>
+                        </li>
+
+                    </ul>
+                    <div class="tab-content pt-2">
+
+                        <div class="tab-pane fade show active profile-overview" id="profile-overview">
+
+                            <h5 class="card-title">Detail BK</h5>
+
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label ">Nama Lengkap</div>
+                                <div class="col-lg-9 col-md-8">Oktaviano Kurniawan</div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">NIP</div>
+                                <div class="col-lg-9 col-md-8">11111111</div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Jenis Kelamin</div>
+                                <div class="col-lg-9 col-md-8">Laki - Laki</div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Alamat</div>
+                                <div class="col-lg-9 col-md-8">Smebarang</div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">No Telpon</div>
+                                <div class="col-lg-9 col-md-8">08xxxx</div>
+                            </div>
+
+                        </div>
+
+                    </div><!-- End Bordered Tabs -->
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+@endsection
