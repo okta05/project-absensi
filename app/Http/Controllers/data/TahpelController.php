30f8a204f65865eecb95c79bb0aa9b47eac764bd
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-12 11:02:13 +0700

    tambah fungsi add, edit, view pada controller tahpel

--- app/Http/Controllers/data/TahpelController.php
+++ app/Http/Controllers/data/TahpelController.php
@@ -8,4 +8,15 @@ use Illuminate\Http\Request;
 class TahpelController extends Controller
 {
     //
+       public function tahpelView() {
+        return view("tampilan.data.tahun_pelajaran.view_tahpel");
+    }
+
+    public function tahpelAdd() {
+        return view("tampilan.data.tahun_pelajaran.add_tahpel");
+    }
+
+    public function tahpelEdit() {
+        return view("tampilan.data.tahun_pelajaran.edit_tahpel");
+    }
 }
