abb0a902b7a42eaa59fba402bdb4cca1e6d95bf7
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-12 10:42:45 +0700

    tambah fungsi add, edit read pada controller mapel

--- app/Http/Controllers/data/MapelController.php
+++ app/Http/Controllers/data/MapelController.php
@@ -8,4 +8,15 @@ use Illuminate\Http\Request;
 class MapelController extends Controller
 {
     //
+    public function mapelView() {
+        return view("tampilan.data.mapel.view_mapel");
+    }
+
+    public function mapelAdd() {
+        return view("tampilan.data.mapel.add_mapel");
+    }
+
+    public function mapeledit() {
+        return view("tampilan.data.mapel.edit_mapel");
+    }
 }
