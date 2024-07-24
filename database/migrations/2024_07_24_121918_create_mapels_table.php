6acd1427a80f2117bc15c00211dd639aca22ff7f
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-07-24 19:23:46 +0700

    buat migration mapel

--- database/migrations/2024_07_24_121918_create_mapels_table.php
+++ database/migrations/2024_07_24_121918_create_mapels_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tahpels', function (Blueprint $table) {
+        Schema::create('mapels', function (Blueprint $table) {
             $table->id();
-            $table->string('nm_mapel')->nullable();
-            $table->string('id_guru')->nullable();
-            $table->string('id_th_pelajaran')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tahpels');
+        Schema::dropIfExists('mapels');
     }
 };
