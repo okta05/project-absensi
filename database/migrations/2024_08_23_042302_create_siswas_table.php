45d186d157cdf0967365dd9f49bb0a9a99e100fd
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-08-23 11:24:51 +0700

    buat table siswa baru

--- database/migrations/2024_08_23_042302_create_siswas_table.php
+++ database/migrations/2024_08_23_042302_create_siswas_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('siswas', function (Blueprint $table) {
             $table->id('id_siswa');
+            $table->string('no_absen')->nullable();
             $table->string('nama')->nullable();
             $table->string('nis')->nullable();
             $table->unsignedBigInteger('id_kelas')->nullable();
