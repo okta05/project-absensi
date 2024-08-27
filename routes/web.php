c8d870258eb77b114bb60c08419c7a98b487c309
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-08-27 20:02:37 +0700

    modif web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Route::middleware([
