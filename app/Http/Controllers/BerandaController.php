ab75afd224415a307857ae599af2f89e289ae1fa
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-09-04 20:53:54 +0700

    modfi tampilan dashboard

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Siswa;
+use App\Models\Admin;
+use App\Models\Kepsek;
+use App\Models\Kurikulum;
+use App\Models\Bk;
+use App\Models\Wakel;
+use App\Models\Guru;
+use App\Models\Kelas;
+use App\Models\Mapel;
 
 class BerandaController extends Controller
 {
     //
     public function index(){
-        return view('tampilan.index');
+        $siswaCount = Siswa::count();
+        $adminCount = Admin::count();
+        $kepsekCount = Kepsek::count();
+        $kurikulumCount = Kurikulum::count();
+        $bkCount = Bk::count();
+        $wakelCount = Wakel::count();
+        $guruCount = Guru::count();
+        $kelasCount = Kelas::count();
+        $mapelCount = Mapel::count();
+        
+        return view('tampilan.index', compact('siswaCount','adminCount','kepsekCount',
+        'kurikulumCount','bkCount','wakelCount','guruCount','kelasCount','mapelCount'));
     }
 }
--- resources/views/tampilan/index.blade.php
+++ resources/views/tampilan/index.blade.php
@@ -3,18 +3,99 @@
 
 <section class="section dashboard">
     <div class="row">
-
-        <!-- Left side columns -->
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             <div class="row">
+                <h1>Dashboard</h1>
 
-                <h1>SELAMAT DATANG PADA DASHBOARD SMPN 2 SEMPU</h1>
-
-
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card bg-primary text-white">
+                                <div class="card-body">
+                                    <h5 class="card-title"><i class="bi bi-people-fill"></i> {{ $siswaCount }}</h5>
+                                    <p class="card-text">Data Siswa</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body">
+                                    <h5 class="card-title"><i class="bi bi-person-fill"></i> {{ $adminCount }}</h5>
+                                    <p class="card-text">Data Admin</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-success text-white">
+                                <div class="card-body">
+                                    <h5 class="card-title"><i class="bi bi-person-fill"></i> {{ $kepsekCount }}</h5>
+                                    <p class="card-text">Data Kepala Sekolah</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-danger text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-person-fill"></i> {{ $kurikulumCount }}</h5>
+                                    <p class="card-text">Data Kurikulum</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-warning text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-person-fill"></i> {{ $bkCount }}</h5>
+                                    <p class="card-text">Data BK</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-info text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-people-fill"></i> {{ $wakelCount }}</h5>
+                                    <p class="card-text">Data Wali Kelas</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-people-fill"></i> {{ $guruCount }}</h5>
+                                    <p class="card-text">Data Guru</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-primary text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-door-open-fill"></i> {{ $kelasCount }}</h5>
+                                    <p class="card-text">Data Kelas</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card bg-success text-white">
+                                <div class="card-body">
+                                <h5 class="card-title"><i class="bi bi-book-fill"></i> {{ $mapelCount }}</h5>
+                                    <p class="card-text">Data Mata Pelajaran</p>
+                                    <a href="#" class="btn btn-light">Lihat detail →</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
+
 </section>
 
 @endsection
