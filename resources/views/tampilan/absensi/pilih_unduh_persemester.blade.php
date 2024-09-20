20e1dacd88738a88bebf72c053e7e9e7cba058b0
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-09-20 11:55:47 +0700

    modifikasi unduh persemester

--- resources/views/tampilan/absensi/pilih_unduh_persemester.blade.php
+++ resources/views/tampilan/absensi/pilih_unduh_persemester.blade.php
@@ -10,7 +10,8 @@
                 <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><i class="bi bi-house-door-fill"></i></a>
                 </li>
                 <li class="breadcrumb-item"><a href="{{ route('mapel.absensi') }}">Pilih Mapel</a></li>
-                <li class="breadcrumb-item"><a href="{{ session('previous_url', route('mapel.absensi')) }}">Absensi</a></li>
+                <li class="breadcrumb-item"><a href="{{ session('previous_url', route('mapel.absensi')) }}">Absensi</a>
+                </li>
                 <li class="breadcrumb-item"><a href="#">Unduh Per Semester</a></li>
             </ol>
         </nav>
@@ -22,7 +23,8 @@
             <div class="card-body">
                 <div class="row mb-2">
                     <div class="col-12 d-flex justify-content-start">
-                        <form action="{{route('unduh_absensi.persemester')}}" method="get" class="d-flex align-items-center">
+                        <form action="{{route('unduh_absensi.persemester')}}" method="get"
+                            class="d-flex align-items-center">
 
                             <!-- Hidden field untuk id_mapel -->
                             <input type="hidden" name="id_mapel" value="{{ $mapel_id }}">
@@ -41,6 +43,39 @@
                             </button>
                         </form>
                     </div>
+                </div>
+            </div>
+
+            <div class="card-body mt-3">
+                <div class="table-responsive">
+                    <!-- Table with stripped rows -->
+                    <table class="table table-striped datatable">
+                        <thead>
+                            <tr>
+                                <th>Tanggal</th>
+                                <th>Jam</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <!-- Tampilkan tanggal hanya sekali per tanggal -->
+
+                                <td>
+
+                                </td>
+
+                                <td></td>
+                                <td>
+                                    <a href="#" class="btn btn-info btn-sm">
+                                        <i class="bi bi-eye"></i> Lihat Detail
+                                    </a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div><!-- End Default Card -->
